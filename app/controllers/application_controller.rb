2c22860ff18785432867806809c0b676664398d6
ogamw <hajime.ogasawara1201@gmail.com>
2020-09-25 23:19:44 +0900

    deviseのデフォルトにないカラムの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configue_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -7,5 +8,14 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']  # 環境変数を読み込む記述に変更
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitixer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitixer.permit(:sign_up, keys: [:familyname])
+    devise_parameter_sanitixer.permit(:sign_up, keys: [:firstname])
+    devise_parameter_sanitixer.permit(:sign_up, keys: [:kana_familiyname])
+    devise_parameter_sanitixer.permit(:sign_up, keys: [:kana_firstnamename])
+    devise_parameter_sanitixer.permit(:sign_up, keys: [:birthday])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, :familyname, :firstname, :kana_familiyname, :kana_firstnamename, :birthday, presence: true
 end
