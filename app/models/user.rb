27170d181e668de688e741dc42cc46b4aff95dd9
ogamw <hajime.ogasawara1201@gmail.com>
2020-09-28 00:40:05 +0900

    メールアドレスとパスワードのテストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   
-  validates :nickname,  :birthday, presence: true
+  validates :nickname, :birthday, presence: true
   validates :familyname, :firstname, presence: true,
             #全角ひらがなカタカナ漢字のみ可
-            format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is must NOT contain any other characters than alphanumerics." }
+            format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is must NOT contain any other characters than full-width characters (kanji, hiragana, katakana)." }
   validates :kana_familyname, :kana_firstname, presence: true,
             #全角カナのみ可
-            format: { with: /\A([ァ-ン]|ー)+\z/, message: "is must NOT contain any other characters than alphanumerics." }
+            format: { with: /\A([ァ-ン]|ー)+\z/, message: "is must NOT contain any other characters than full-width characters (katakana)." }
+
+  validates :email, presence: true,
+            #@が含まれていれば可
+            format: { with: VALID_EMAIL_REGEX, message: "is improper format."}
+
+  validates :password, presence: true,
+            #半角英数字混合のみ可
+            format: { with: VALID_PASSWORD_REGEX, message: "is must contain both letters and numbers."}
 end
