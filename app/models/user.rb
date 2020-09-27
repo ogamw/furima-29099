332c3f92f0f34520ac18d93cf2ebce55ffb54235
ogamw <hajime.ogasawara1201@gmail.com>
2020-09-28 00:41:14 +0900

    ルボコップの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,22 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
-  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+
   validates :nickname, :birthday, presence: true
   validates :familyname, :firstname, presence: true,
-            #全角ひらがなカタカナ漢字のみ可
-            format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is must NOT contain any other characters than full-width characters (kanji, hiragana, katakana)." }
+                                     # 全角ひらがなカタカナ漢字のみ可
+                                     format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is must NOT contain any other characters than full-width characters (kanji, hiragana, katakana).' }
   validates :kana_familyname, :kana_firstname, presence: true,
-            #全角カナのみ可
-            format: { with: /\A([ァ-ン]|ー)+\z/, message: "is must NOT contain any other characters than full-width characters (katakana)." }
+                                               # 全角カナのみ可
+                                               format: { with: /\A([ァ-ン]|ー)+\z/, message: 'is must NOT contain any other characters than full-width characters (katakana).' }
 
   validates :email, presence: true,
-            #@が含まれていれば可
-            format: { with: VALID_EMAIL_REGEX, message: "is improper format."}
+                    # @が含まれていれば可
+                    format: { with: VALID_EMAIL_REGEX, message: 'is improper format.' }
 
   validates :password, presence: true,
-            #半角英数字混合のみ可
-            format: { with: VALID_PASSWORD_REGEX, message: "is must contain both letters and numbers."}
+                       # 半角英数字混合のみ可
+                       format: { with: VALID_PASSWORD_REGEX, message: 'is must contain both letters and numbers.'}
 end
