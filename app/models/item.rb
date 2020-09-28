104c8fdcfeca7c92d565f5f50169e6b9ee33210c
ogamw <hajime.ogasawara1201@gmail.com>
2020-09-29 07:24:23 +0900

    ActiveHashファイルとのアソシエーションと、バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,12 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :condition, :postage, :shipping_area, :days_to_ship
   has_one_attached :image
+
+  with_options presence: true do
+    validates :item_name,:text,:price
+  end
+  with_options numericality: { other_than: 1 } do
+    validates :category_id,:condition_id,:postage_id,:shipping_area_id,:days_to_ship_id
+  end
 end
