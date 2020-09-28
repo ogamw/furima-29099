be58a4d570b19067cccebb069c3594ec93737c22
ogamw <hajime.ogasawara1201@gmail.com>
2020-09-29 07:33:23 +0900

    items_controllerの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,9 +7,18 @@ class ItemsController < ApplicationController
     @items = Item.new
   end
 
+  def create
+    @items = Item.new(items_params)
+    if @items.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def items_params
-    params.require(:items).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:items).permit(:content, :image, :text, :category_id, :condition_id,:postage_id, :shipping_area_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
   end
 end
