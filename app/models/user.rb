def8013625736a616d9054172877ff97c97e856f
ogamw <hajime.ogasawara1201@gmail.com>
2020-09-28 11:30:34 +0900

    ルボコップによる修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,18 +7,18 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
 
   with_options presence: true do
-       validates :nickname, :birthday
-       validates :familyname, :firstname,
-                                          # 全角ひらがなカタカナ漢字のみ可
-                                          format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is must NOT contain any other characters than full-width characters (kanji, hiragana, katakana).' }
-       validates :kana_familyname, :kana_firstname,
-                                                 # 全角カナのみ可
-                                                 format: { with: /\A([ァ-ン]|ー)+\z/, message: 'is must NOT contain any other characters than full-width characters (katakana).' }
-       validates :email,
-                     # @が含まれていれば可
-                     format: { with: VALID_EMAIL_REGEX, message: 'is improper format.' }
-       validates :password,
-                            # 半角英数字混合のみ可
-                            format: { with: VALID_PASSWORD_REGEX, message: 'is must contain both letters and numbers.' }
+    validates :nickname, :birthday
+    validates :familyname, :firstname,
+              # 全角ひらがなカタカナ漢字のみ可
+              format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is must NOT contain any other characters than full-width characters (kanji, hiragana, katakana).' }
+    validates :kana_familyname, :kana_firstname,
+              # 全角カナのみ可
+              format: { with: /\A([ァ-ン]|ー)+\z/, message: 'is must NOT contain any other characters than full-width characters (katakana).' }
+    validates :email,
+              # @が含まれていれば可
+              format: { with: VALID_EMAIL_REGEX, message: 'is improper format.' }
+    validates :password,
+              # 半角英数字混合のみ可
+              format: { with: VALID_PASSWORD_REGEX, message: 'is must contain both letters and numbers.' }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     end
 
     nickname              { Faker::Name.name }
-    email                 {Faker::Internet.free_email}
+    email                 { Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 8)}
     password_confirmation {password}
     familyname            {person.last.kanji}
