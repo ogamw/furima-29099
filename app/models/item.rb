7699725084686903c09cc70131c6187fb0bce1e6
ogamw <hajime.ogasawara1201@gmail.com>
2020-09-30 02:09:53 +0900

    itemモデルの記述の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,14 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category, :condition, :postage, :shipping_area, :days_to_ship
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :postage
+  belongs_to_active_hash :shipping_area
+  belongs_to_active_hash :days_to_ship
   has_one_attached :image
 
-  with_options presence: true {message: 'can`t be blank'} do
-    validates :item_name,:text, :category, :condition, :postage, :shipping_area, :days_to_ship, :price
+  with_options presence: true do
+    validates :item_name, :text, :category, :condition, :postage, :shipping_area, :days_to_ship, :price
   end
   with_options numericality: { other_than: 1 ,message: 'Select.'} do
     validates :category_id,:condition_id,:postage_id,:shipping_area_id,:days_to_ship_id
