817e9b638300779c2fca39e0f654254990cc3704
ogamw <hajime.ogasawara1201@gmail.com>
2020-10-02 18:09:23 +0900

    テスト用のデータを追加

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,8 +2,12 @@ FactoryBot.define do
   factory :item do
     item_name {Faker::Name.name}
     text {Faker::Lorem.sentence}
-    price {Faker::Number.number}
-
+    price {Faker::Number.number} 
+    category_id {2}
+    condition_id{2}
+    postage_id {2}
+    shipping_area_id {2}
+    days_to_ship_id {2}
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.jpg'), filename: 'test_image.jpg')
     end
