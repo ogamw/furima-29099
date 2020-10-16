f9e215b2f18cda6db53140167807e6996a04b560
ogamw <hajime.ogasawara1201@gmail.com>
2020-10-16 17:41:16 +0900

    ルボコップによる修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,8 +6,8 @@ FactoryBot.define do
 
     nickname              { Faker::Name.name }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 8)}
-    password_confirmation {password}
+    password              { Faker::Internet.password(min_length: 8) }
+    password_confirmation { password}
     familyname            {person.last.kanji}
     firstname             {person.first.kanji}
     kana_familyname       {person.last.katakana}
